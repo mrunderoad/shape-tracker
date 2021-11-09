1a5c35687159f1d78aa0b25f12811525b39b90e4
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-11-08 16:04:21 -0800

    Followed instructions to complete environment

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import $ from 'jQuery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Triangle from './triangle.js';
+import './css/styles.css';
+
+$(document).ready(function() {
+  $('#triangle-checker-form').submit(function(event) {
+    event.preventDefault();
+    /*eslint-disable no-debugger*/
+    debugger;
+    /*eslint-enable no-debugger*/
+    const length1 = $('#length1').val();
+    const length2 = $('#length2').val();
+    const length3 = $('#length3').val();
+    const triangle = new Triangle(length1, length2, length3)
+    const response = triangle.checkType();
+    $('#response').append("<p>" + response + "</p>");
+  });
+});
